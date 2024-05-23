4a9b8d0f93b5d4e01fca50f0bfcbe56e27de1674
vicsnet <adesanmiadebisi@gmail.com>
2024-05-23 16:56:17 +0100

    slasher function added

--- src/ProofRegistry.sol
+++ src/ProofRegistry.sol
@@ -96,6 +96,10 @@ contract ProofRegistry {
         bool isValid
     ) external {
         address proofVerifier = msg.sender;
+        // Checking if sender is an operator on eigen layer
+        // 0x39053D51B77DC0d36036Fc1fCc8Cb819df8Ef37A Eigenlayer main contract
+        bool valid = IEigenLayer(0x39053D51B77DC0d36036Fc1fCc8Cb819df8Ef37A).isOperator(proofVerifier);
+       require(valid == true, "NOT_AN_OPERATOR");
         bytes memory proof = abi.encode(_publicInputs, _proof, _recursiveAggregationInput);
         bytes32 proofHash = keccak256(proof);
         ProofVerificationClaim memory proofWitness = isValidProof[proofHash];
@@ -187,6 +191,8 @@ contract ProofRegistry {
     }
 
     function slash(address maliciousProposer) internal {
+        address slasherContract = 0xD92145c07f8Ed1D392c1B88017934E301CC1c3Cd;
+        IEigenLayerSlasher(slasherContract).optIntoSlashing(maliciousProposer);
         // penalises the maliciousProposer and evicts them from the precompile service
         // eigenlayer slashing
     }
@@ -202,4 +208,13 @@ interface IVerifier {
 
 interface IGetVerifier {
     function getVerifier() external returns (address);
+}
+interface IEigenLayer{
+    function isOperator(address _operator) external returns(bool);
+}
+
+interface IEigenLayerSlasher{
+    function optIntoSlashing () external {
+
+    }
 }
